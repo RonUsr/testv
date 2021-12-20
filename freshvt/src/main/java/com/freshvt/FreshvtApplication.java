6e694cd280cc1e5bfa29a25235010d2030aba552
RonaldMSn <rmaldonado@soaint.com>
2021-12-20 15:57:04 -0500

    Initial check-in app test votes

---
+++ freshvt/src/main/java/com/freshvt/FreshvtApplication.java
@@ -0,0 +1,13 @@
+package com.freshvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshvtApplication.class, args);
+	}
+
+}
